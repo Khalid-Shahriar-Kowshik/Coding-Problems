6975f68dc2f902b2c701b2e836bea312a29be346
Khalid Shahriar Kowshik <khalidkowshikcse@gmail.com>
2024-11-08 11:59:00 +0600

    leetcode prob-2

---
+++ LeetCode/[1] Contains Duplicate/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        contains=set()
+
+        for i in nums:
+            if i in contains:
+                return True
+            else:
+                contains.add(i)
+        return False
